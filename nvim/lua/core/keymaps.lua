194d6ac4dbbb44e78ec21163e1773e7eecea4e4d
chenshi <chenshi@krutt.org>
2025-01-19 19:37:54 +0800

    Update

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -17,3 +17,10 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- 启用nvim-tree
 keymap.set("n", "<leader>tr", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- 启用nvim-telescope快捷键
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find Buffers" })
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find Help" })
+
---
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                    },
+                    prompt_prefix = " ",
+                    selection_caret = " ",
+                    path_display = { "smart" },
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown", -- 使用下拉样式
+                    },
+                },
+                extensions = {
+                    -- 添加扩展的配置
+                },
+            })
+
+            -- 加载扩展（如果需要）
+            -- telescope.load_extension('fzf')
+        end,
+    },
+}
+
