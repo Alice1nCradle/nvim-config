20ec96210f00ae2128691181282742e385619000
Alice1nCradle <chenshi@krutt.org>
2025-01-28 23:26:22 +0800

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,6 @@
 local opt = vim.opt
+local api = vim.api
+local g = vim.g
 
 -- 行号
 opt.relativenumber = true
@@ -29,3 +31,43 @@ opt.smartcase = true
 -- 外观
 opt.termguicolors = true
 opt.signcolumn = "yes"
+
+-- wrap the line via win_width
+local function set_dynamic_wrap()
+  -- get win_width
+  local win_width = vim.api.nvim_win_get_width(0)
+  -- set dynamic colorcolumn
+  if win_width > 80 then
+    opt.colorcolumn = tostring(win_width - 5)
+  else
+    opt.colorcolumn = "0"
+  end
+end
+
+-- automation
+api.nvim_create_autocmd("VimResized", {
+  callback = set_dynamic_wrap
+})
+
+set_dynamic_wrap()
+
+opt.wrap = true
+opt.linebreak = true
+opt.textwidth = 0
+
+-- configure vimtex
+g.vimtex_compiler_method = 'latexmk'
+g.vimtex_compiler_latexmk = {
+  options = {
+    "-pdf",
+    "-shell-escape",
+    "-synctex=1",
+    "-interaction=nonstopmode"
+  }
+}
+
+-- auto compiling *.tex
+api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.tex",
+  command = "VimtexCompile"
+})
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { "hrsh7th/cmp-nvim-lsp" },
     event = "BufReadPre", -- 延迟加载
     config = function()
       local lspconfig = require("lspconfig")
@@ -29,6 +30,29 @@ return {
       lspconfig.volar.setup({
         on_attach = on_attach,
         filetypes = { "vue" }, -- 明确支持的文件类型
+      })
+      -- configure texlab
+      lspconfig.texlab.setup({
+        settings = {
+        texlab = {
+          latex = {
+            build = {
+              executable = "latexmk",
+              args = {
+                "-synctex=1",
+                "-interaction=nonstopmode",
+                "-file-line-error",
+                "-pdf",
+                "%f"
+              }
+            }
+          },
+          view = {
+            method = "sumatrapdf",
+            forwardSearch = true
+          }
+        }
+      }
       })
     end,
 }
